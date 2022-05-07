fbb1b81b769f9472878d222158de82651cf3109b
Michael <m@cadot.eu>
2022-05-07 18:35:58 +0200

    modif tests

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,21 +2,22 @@
 
 namespace App\Controller;
 
-use App\Service\FileUploader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Service\base\FixtureHelper;
+use App\Service\base\FileUploader;
 
 class HomeController extends AbstractController
 {
 
     #[Route('/', name: 'home_index')]
-    public function index(Request $request): Response
+    public function index(Request $request, FixtureHelper $fixtureHelper): Response
     {
         /* ------------------------------- simulateurs ------------------------------ */
-        return $this->render('home/index.html.twig', []);
+        return $this->render('home/index.html.twig', ['testhelper' => $fixtureHelper::generate(['type' => 'phrase'])]);
     }
 
 
--- src/DataFixtures/AdminFixtures.php
+++ src/DataFixtures/AdminFixtures.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\DataFixtures;
+namespace App\DataFixtures\base;
 
 use App\Entity\Compte;
 use App\Entity\Parametres;
